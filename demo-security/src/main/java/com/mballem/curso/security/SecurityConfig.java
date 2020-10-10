d7062737245462c83d96a29bd37f4694ece33456
cerchiariluiza <cerchiariluiza>
2020-10-10 19:03:53 -0300

    segurança com formulario

--- demo-security/src/main/java/com/mballem/curso/security/SecurityConfig.java
+++ demo-security/src/main/java/com/mballem/curso/security/SecurityConfig.java
@@ -3,17 +3,34 @@ package com.mballem.curso.security;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    //Rodar e ver que com barra funciona
+
+	    
 		http.authorizeRequests()
-		.antMatchers("/home", "/webjars/**", "/", "/css/**", "/image/**").permitAll()
-		.anyRequest().authenticated();
+		// acessos públicos liberados
+		.antMatchers("/webjars/**", "/css/**", "/image/**", "/js/**").permitAll()
+		.antMatchers("/login", "/home").permitAll()
 		
-	}
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/", true)
+			.failureUrl("/login-error")
+			.permitAll()
+		.and()
+			.logout()
+			.logoutSuccessUrl("/");
+}
 
 }
+
+
+
